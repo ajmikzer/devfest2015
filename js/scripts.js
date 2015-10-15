0536363606ba9d555067b297bf4026510ff9aa08
ajmikzer <scroller20@mail.ru>
2015-10-15 10:38:12 +0300

    masonry removed

--- js/scripts.js
+++ js/scripts.js
@@ -20,23 +20,6 @@ $(document).ready(function(){
 
 		});
 
-		// Masonry
-		var $container = $('.masonry');
-        $container.imagesLoaded( function() {
-        	$container.isotope({
-        		itemSelector: '.masonry-item',
-				layoutMode: 'masonry',
-				resizesContainer:false,
-				percentPosition: true,
-				masonry: { columnWidth: '.work-img', gutter: 6 }
-			});
-		});
-
-		// Trigger Resize
-		$(window).trigger("resize");
-
-	});
-
 
 	// Text Rotator
 	$(".rotate").textrotator({
